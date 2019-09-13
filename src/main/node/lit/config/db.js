e4f744d047eae4f6980d047a4921cee309361efb
imtaarun <31630535+imtaarun@users.noreply.github.com>
2019-09-13 19:57:31 +0530

    Update db.js

--- src/main/node/lit/config/db.js
+++ src/main/node/lit/config/db.js
@@ -1,9 +1,6 @@
 const mongo = require('mongoose');
 
-var localDb = 'mongodb://localhost:27017/lit';
-var serverDb = 'mongodb+srv://AdminUser:Imacoder7@lit-s8rma.mongodb.net/test?retryWrites=true&w=majority';
-
-mongo.connect(serverDb);
+mongo.connect('mongodb+srv://AdminUser:Imacoder7@lit-s8rma.mongodb.net/test?retryWrites=true&w=majority');
 
 var db = mongo.connection;
 db.on('error',function(){
